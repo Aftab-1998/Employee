ac424be414c72d6e06cc8f6f31fb0b2e011a2dcb
Aftab-1998 <alam41976@gmail.com>
2021-11-28 19:24:10 +0530

    [Aftab3] Add ; Added Implemented

--- Employee/EmployeeWage.cs
+++ Employee/EmployeeWage.cs
@@ -11,6 +11,7 @@ namespace Employee
         //constant
         int IS_FULL_TIME = 1;
         int EMP_RATE_PER_HOUR = 20;
+        int IS_PART_TIME = 0;
         //variables
         int empHrs = 0;
         int empWage = 0;
@@ -48,7 +49,28 @@ namespace Employee
                 return empWage;
 
             }
+        public void PartTime()
+        {
+            Random random = new Random();
+            int empcheck = random.Next(0, 3);
+            if (empcheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empcheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
         }
+    }
+
+
 
     
 }
